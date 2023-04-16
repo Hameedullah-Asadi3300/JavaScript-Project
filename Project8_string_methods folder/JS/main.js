60dc7d76ca51a963a22c88d023ac03bcab949319
Hameedullah Asadi <hameedasade20@gmail.com>
2023-04-16 15:52:23 -0700

    worked on concatenation

---
+++ Project8_string_methods folder/JS/main.js
@@ -0,0 +1,16 @@
+
+
+//Cconcatenation
+function my_Full_Sentence() {
+    var part_1 = "I want to become a ";
+    var part_2 = "skillful front-end Developer, ";
+    var part_3 = "in next six months.";
+    var whole_sentence = part_1 + part_2 + part_3;
+    document.getElementById("concat").innerHTML = whole_sentence;
+}
+
+
+
+
+
+
