8a57d98f519653b68f4b84d08775bac007418c78
Hameedullah Asadi <hameedasade20@gmail.com>
2023-04-17 10:25:44 -0700

    slice, toUpeerCase, toFixed, search and a others

--- Project8_string_methods folder/JS/main.js
+++ Project8_string_methods folder/JS/main.js
@@ -1,6 +1,6 @@
 
 
-//Cconcatenation
+//Concatenation
 function my_Full_Sentence() {
     var part_1 = "I want to become a ";
     var part_2 = "skillful front-end Developer, ";
@@ -10,7 +10,57 @@ function my_Full_Sentence() {
 }
 
 
+//Slice Method
+function slice_Method() {
+    var sentence = "All works and no play makes Johny a dull boy.";
+    var section = sentence.slice(27,33);
+    document.getElementById("slice").innerHTML = section; 
+}
+
+//ToUpperCase Method as Function
+    function capitalLetter() {
+        var sentence_1 = "Hello world";
+        var sentence_2 = sentence_1.toUpperCase();
+        document.getElementById("upper").innerHTML = sentence_2;
+    }
+    
+
+//ToUpperCase Method as variable
+    var text = "Hello friend";
+    var result = text.toUpperCase();
+    document.getElementById("capital").innerHTML = result;
 
 
+//Search Method
+function searchMethod() {
+    let text = "Mr. Blue has a blue house"
+    let position = text.search("Blue");
+    document.getElementById("demo").innerHTML = position;
+    }
 
+
+    //Numbers to String Method
+function string_Method() {
+    var x = 182;
+    document.getElementById("Numbers_to_String").innerHTML = x.toString();
+}
+    
+
+//toPrecision () Method
+function precision_Method() {
+    var x = 12938.3012987376113;
+    document.getElementById("precision").innerHTML = x.toPrecision(10);
+}
+
+
+//toFixed Mthod
+var x = 4.97543;
+var n = x.toFixed();
+document.getElementById("demo").innerHTML = n;
+
+
+//2nd tofixed Method
+var y = 9.98543363;
+var m = y.toFixed();
+document.getElementById("demo_1").innerHTML = m;
 
